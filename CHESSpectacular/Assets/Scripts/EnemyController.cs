f0ce359c98c66cb07edb328be7288f469bf0e80f
LalitoR-jpg <laloro08@hotmail.com>
2022-04-05 23:51:35 -0500

    05 de Abril

---
+++ CHESSpectacular/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform player;
+    public void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+    public void Update()
+    {   
+        transform.LookAt(player);    
+
+    }
+}
