cadbfdbac7c84e7f77dc5f891809b8dc8cbfe49f
LalitoR-jpg <laloro08@hotmail.com>
2022-04-06 13:46:36 -0500

    06 de Abril

--- CHESSpectacular/Assets/Scripts/EnemyController.cs
+++ CHESSpectacular/Assets/Scripts/EnemyController.cs
@@ -1,17 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+    public NavMeshAgent agent;
     public Transform player;
-    public void Awake()
-    {
+    public LayerMask whatIsGround, whatIsPlayer;
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+    private void Awake(){
         player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
     }
-    public void Update()
-    {   
-        transform.LookAt(player);    
-
+    private void Update(){
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        if(!playerInSightRange) Patroling();
+        else ChasePlayer();
     }
+    private void Patroling(){
+        if(!walkPointSet) SearchWalkPoint();
+        else 
+            agent.SetDestination(walkPoint);
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+        if(distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+    private void SearchWalkPoint(){
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        if(Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+    private void ChasePlayer() {
+        agent.SetDestination(player.position);
+    }   
 }
